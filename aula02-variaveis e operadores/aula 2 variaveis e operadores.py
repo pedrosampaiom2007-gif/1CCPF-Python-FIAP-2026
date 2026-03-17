741e2fdb13975c196cc6f388613398702bdc559a
pedrosampaiom207-gif <pedrosampaiom2007@gmail.com>
2026-03-17 11:41:09 -0300

    aula 02

---
+++ aula02-variaveis e operadores/aula 2 variaveis e operadores.py
@@ -0,0 +1,27 @@
+print('olá mundo')
+print(7+4)
+print('7'+"4"
+          "")# CONCATENAÇÃO DE STRINGS
+
+#VARIÁVEIS
+nome= ("alexxandre")
+idade= 67
+peso= 78
+print(nome,idade,peso)
+
+print ('oii',nome,idade,peso)
+print (f'oii {nome}')
+
+nome=input ( 'qual é o seu nome')
+idade= input ('quantos anos voce tem')
+peso= input  ('qual o seu peso')
+
+print(nome,idade,peso)
+
+nome=input ('qual o seu nome')
+print(f' seja bem vindo'{nome})
+
+dia= input ('qual a data de nascimento')
+mes=input ('qual o mês de nascimento ')
+ano= input ('qual o ano de nascimento ')
+
